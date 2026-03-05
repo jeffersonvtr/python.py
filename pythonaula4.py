c69ccb923c528c88513cb4da5cb735be95d77a47
jeffersonvtr <jefferson.zambao@escola.pr.gov.br>
2026-03-05 11:34:10 -0300

    Create pythonaula4.py

---
+++ pythonaula4.py
@@ -0,0 +1,5 @@
+#int = números inteiros, ex 6, 4, -7
+#float = números reais com virgula. ex 2.1, 3.4
+#bool = real ou falso, ex true or false(sempre usae número com virgula)
+#str = aracter de texto, ex 'ola', '2.2'
+#para saber o tipo, basta usar print(type(variavel))
