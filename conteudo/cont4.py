5251f051bc69a8d45d2305b3440eff4ddd1a3bd9
jeffersonvtr <jefferson.zambao@escola.pr.gov.br>
2026-03-11 11:34:56 -0300

    Create cont4.py

---
+++ conteudo/cont4.py
@@ -0,0 +1,13 @@
+#+ é utilizado como operador de adição
+
+#- é utilizado como sinal de subtração
+
+#* é utilizado para fazer a multiplicação
+
+#/ é utilizada para fazer a divisão
+
+#** é utilizado para fazer a exponenciação
+
+#// é utilizado para fazer a divisão inteira
+
+#% é utilizada para o resto da divisão
