3399d5f1bc75a588392126774d0d8bff1056f498
jeffersonvtr <jefferson.zambao@escola.pr.gov.br>
2026-03-16 10:52:08 -0300

    Update cont6.py

--- conteudo/cont6.py
+++ conteudo/cont6.py
@@ -6,3 +6,12 @@ Python
 
 nome = input('Qual é o seu nome?')
 print('prazer em conhece-lo {:20}!'.format(nome))
+OPERADORES
+Podemos escrever um resultado na quantidade de caracteres específica ao colocar o valor dentro da máscara após escrever
+
+Código:
+
+Python
+
+nome = input('Qual é o seu nome?')
+print('prazer em conhece-lo {:20}!'.format(nome))
