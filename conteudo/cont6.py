04dcc52fce0eae1bbd5d8d263de5b85df8c46c34
jeffersonvtr <jefferson.zambao@escola.pr.gov.br>
2026-03-16 10:47:48 -0300

    Update cont6.py

--- conteudo/cont6.py
+++ conteudo/cont6.py
@@ -1,4 +1,8 @@
-#5 + 3 * 2 ==
-#3 * 5 + 4 ** 2 ==
-#3 * (5 + 4) ** 2 ==
-#O Python é utilizado em cálculos científicos e o tamanho da memoria ou seja conseguimos chegar a números extremamente grandes
+Podemos escrever um resultado na quantidade de caracteres específica ao colocar o valor dentro da máscara após escrever
+
+Código:
+
+Python
+
+nome = input('Qual é o seu nome?')
+print('prazer em conhece-lo {:20}!'.format(nome))
