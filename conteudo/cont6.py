e87bc3f0a23a97b35b8543ae3552dec40a18e8ae
jeffersonvtr <jefferson.zambao@escola.pr.gov.br>
2026-03-16 10:40:34 -0300

    Create cont6.py

---
+++ conteudo/cont6.py
@@ -0,0 +1,4 @@
+#5 + 3 * 2 ==
+#3 * 5 + 4 ** 2 ==
+#3 * (5 + 4) ** 2 ==
+#O Python é utilizado em cálculos científicos e o tamanho da memoria ou seja conseguimos chegar a números extremamente grandes
