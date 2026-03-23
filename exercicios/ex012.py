c48d79d2e209b7f1f6b1e6a0bdc3abed78ada9ab
jeffersonvtr <jefferson.zambao@escola.pr.gov.br>
2026-03-23 10:37:55 -0300

    Create ex012.py

---
+++ exercicios/ex012.py
@@ -0,0 +1,5 @@
+l = float(input('digite a largura da sua parede: '))
+a = float(input('digite a altura: '))
+tt = l * a
+print('a largura da sua parede é {}, altura {} e a area total é {}'.format(l, a, tt))
+print('vai ser preciso {} de tinta.'.format(tt / 2))
