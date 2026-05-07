18df834ca7b6438748f44331dd1639259308498f
jeffersonvtr <jefferson.zambao@escola.pr.gov.br>
2026-05-07 08:05:55 -0300

    Create ex20.py

---
+++ exercicios/ex20.py
@@ -0,0 +1,7 @@
+nome = str(input('digite seu nome')).strip()
+dividido = nome.split()
+print('analisando seu nome...')
+print('seu nome em maiusculo é {}'.format(nome.upper()))
+print('seu nome em minusculo é {}'.format(nome.lower()))
+print('seu nome tem ao todo {} letras'.format(len(nome) - nome.count(' ')))
+print('seu primeiro nome é {} e ele tem {} letras'.format(dividido[0], len(dividido[0])))
