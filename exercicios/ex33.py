09c5c29c6259199f5c0fd54af00142c190429d1d
jeffersonvtr <jefferson.zambao@escola.pr.gov.br>
2026-05-18 10:45:03 -0300

    Create ex33.py

---
+++ exercicios/ex33.py
@@ -0,0 +1,9 @@
+n1 = int(input('digite um número'))
+n2 = int(input('digite outro'))
+n3 = int(input('digite outro'))
+if n1 > n2 and n1 > n3:
+ print(n1)   
+elif n2 > n1 and n2 > n3:
+ print(n2)
+elif n3 > n1 and n3 > n2:
+ print(n3)
